07152fcdf49887f818f23883a958f77b579bd67c
Stephen Reynolds <mail@stephenreynolds.me>
2017-08-08 16:58:30 -0400

    Back to using flags for moving between rooms

--- src/creepUtils/creepUtils.ts
+++ src/creepUtils/creepUtils.ts
@@ -11,19 +11,6 @@ export function moveTo(creep: Creep, target: RoomPosition) {
   });
 }
 
-export function moveToRoom(creep: Creep, destination: Room) {
-  const route = Game.map.findRoute(creep.room, destination) as Array<{ exit: number, room: string }>;
-  if (route.length > 0) {
-    const exit = creep.pos.findClosestByRange(route[0].exit) as RoomPosition;
-    moveTo(creep, exit);
-  }
-}
-
-export function constructSite(creep: Creep, site: ConstructionSite) {
-  if (creep.room !== site.room) {
-    moveToRoom(creep, site.room!);
-  }
-  else if (creep.build(site) === ERR_NOT_IN_RANGE) {
-    moveTo(creep, site.pos);
-  }
+export function moveToRoom(creep: Creep, roomName: string) {
+  moveTo(creep, Game.flags[roomName + "-UF"].pos);
 }

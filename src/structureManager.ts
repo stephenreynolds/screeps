ca90fb44e7085170bef10f4a09cb6b9bec5d022d
Stephen Reynolds <mail@stephenreynolds.me>
2017-08-18 00:54:11 -0400

    No more limit on construction site count

--- src/structureManager.ts
+++ src/structureManager.ts
@@ -1,35 +1,31 @@
 import { RoomData } from "./roomData";
 
 export function run() {
-  let siteCount = RoomData.sites.length;
-  if (siteCount < 10) {
-    const roomPlan = require(`json/rooms/${RoomData.room.name}.json`).rcl[RoomData.room.controller!.level]["buildings"];
+  const roomPlan = require(`json/rooms/${RoomData.room.name}.json`).rcl[RoomData.room.controller!.level]["buildings"];
 
-    // For each building...
-    for (const key in roomPlan) {
-      // Get each position...
-      for (const position in roomPlan[key]["pos"]) {
-        const pos = new RoomPosition(roomPlan[key]["pos"][position].x,
-          roomPlan[key]["pos"][position].y, RoomData.room.name);
+  // For each building...
+  for (const key in roomPlan) {
+    // Get each position...
+    for (const position in roomPlan[key]["pos"]) {
+      const pos = new RoomPosition(roomPlan[key]["pos"][position].x,
+        roomPlan[key]["pos"][position].y, RoomData.room.name);
 
-        // Check if structure or construction site already exists here.
-        const structures = _.filter(pos.look(), (r) => {
-          if (r.type === "structure") {
-            return r.structure && r.structure.structureType === key;
-          }
-          else if (r.type === "constructionSite") {
-            return r.constructionSite && r.constructionSite.structureType === key;
-          }
-          else {
-            return false;
-          }
-        });
-
-        // Create construction site if nothing is here.
-        if (structures.length === 0 && siteCount <= 10) {
-          pos.createConstructionSite(key);
-          siteCount++;
+      // Check if structure or construction site already exists here.
+      const structures = _.filter(pos.look(), (r) => {
+        if (r.type === "structure") {
+          return r.structure!.structureType === key;
         }
+        else if (r.type === "constructionSite") {
+          return r.constructionSite!.structureType === key;
+        }
+        else {
+          return false;
+        }
+      });
+
+      // Create construction site if nothing is here.
+      if (structures.length === 0) {
+        pos.createConstructionSite(key);
       }
     }
   }

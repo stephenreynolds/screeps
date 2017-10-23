d7245bb4e2bba4951d546333e4e8451b2b8f8ce3
Stephen Reynolds <mail@stephenreynolds.me>
2017-10-23 17:03:51 -0400

    Change function to lambda to follow style

--- src/processTypes/creepActions/move.ts
+++ src/processTypes/creepActions/move.ts
@@ -2,67 +2,67 @@ import { Process } from "../../os/process";
 
 interface MoveMetaData
 {
-  creep: string;
-  pos: {
-    x: number
-    y: number
-    roomName: string
-  };
-  range: number;
+    creep: string;
+    pos: {
+        x: number
+        y: number
+        roomName: string
+    };
+    range: number;
 }
 
 export class MoveProcess extends Process
 {
-  public metaData: MoveMetaData;
-  public type = "move";
+    public metaData: MoveMetaData;
+    public type = "move";
 
-  public run()
-  {
-    const creep = Game.creeps[this.metaData.creep];
-
-    if (!creep || !this.metaData.pos)
+    public run()
     {
-      this.completed = true;
-      this.resumeParent();
-      return;
-    }
+        const creep = Game.creeps[this.metaData.creep];
 
-    const target = new RoomPosition(this.metaData.pos.x, this.metaData.pos.y, this.metaData.pos.roomName);
-
-    if (creep.fatigue === 0)
-    {
-      if (creep.pos.inRangeTo(target, this.metaData.range) || creep.pos.isEqualTo(target))
-      {
-        this.completed = true;
-        this.resumeParent();
-      }
-      else
-      {
-        creep.moveTo(target, {
-          visualizePathStyle: {
-            fill: "transparent",
-            lineStyle: "dashed",
-            opacity: .1,
-            stroke: "#fff",
-            strokeWidth: .15
-          }
-        });
-      }
-    }
-    else
-    {
-      let decreasePerTick = 0;
-      _.forEach(creep.body, function(part)
-      {
-        if (part.type === MOVE)
+        if (!creep || !this.metaData.pos)
         {
-          decreasePerTick += 2;
+            this.completed = true;
+            this.resumeParent();
+            return;
         }
-      });
 
-      const ticks = Math.ceil(creep.fatigue / decreasePerTick);
+        const target = new RoomPosition(this.metaData.pos.x, this.metaData.pos.y, this.metaData.pos.roomName);
 
-      this.suspend = ticks;
+        if (creep.fatigue === 0)
+        {
+            if (creep.pos.inRangeTo(target, this.metaData.range) || creep.pos.isEqualTo(target))
+            {
+                this.completed = true;
+                this.resumeParent();
+            }
+            else
+            {
+                creep.moveTo(target, {
+                    visualizePathStyle: {
+                        fill: "transparent",
+                        lineStyle: "dashed",
+                        opacity: .1,
+                        stroke: "#fff",
+                        strokeWidth: .15
+                    }
+                });
+            }
+        }
+        else
+        {
+            let decreasePerTick = 0;
+            _.forEach(creep.body, (part) =>
+            {
+                if (part.type === MOVE)
+                {
+                    decreasePerTick += 2;
+                }
+            });
+
+            const ticks = Math.ceil(creep.fatigue / decreasePerTick);
+
+            this.suspend = ticks;
+        }
     }
-  }
 }

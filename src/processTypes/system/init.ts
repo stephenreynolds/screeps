783d930e97206f6e6de2080dee8982d99236c7ab
Stephen Reynolds <mail@stephenreynolds.me>
2017-10-22 22:44:35 -0400

    Do not create processes for non-owned rooms

--- src/processTypes/system/init.ts
+++ src/processTypes/system/init.ts
@@ -8,50 +8,53 @@ import { SuspensionProcess } from "./suspension";
 
 export class InitProcess extends Process
 {
-  public type = "init";
+    public type = "init";
 
-  /** Run the init process */
-  public run()
-  {
-    const proc = this;
-
-    if (Game.cpu.bucket > 3000)
+    /** Run the init process */
+    public run()
     {
-      this.kernel.limit = (Game.cpu.limit + 500) - 20;
-    }
+        const proc = this;
 
-    for (const name in Memory.creeps)
-    {
-      if (!Game.creeps[name])
-      {
-        delete Memory.creeps[name];
-      }
-    }
+        if (Game.cpu.bucket > 3000)
+        {
+            this.kernel.limit = (Game.cpu.limit + 500) - 20;
+        }
 
-    _.forEach(Game.rooms, function(room)
-    {
-      proc.kernel.addProcess(RoomDataProcess, "roomData-" + room.name, 99, {
-        roomName: room.name
-      });
+        for (const name in Memory.creeps)
+        {
+            if (!Game.creeps[name])
+            {
+                delete Memory.creeps[name];
+            }
+        }
 
-      if (!proc.kernel.getProcessByName("em-" + room.name))
-      {
-        proc.kernel.addProcess(EnergyManagementProcess, "em-" + room.name, 50, {
-          roomName: room.name
+        _.forEach(Game.rooms, (room) =>
+        {
+            if (room.controller && room.controller.my)
+            {
+                proc.kernel.addProcess(RoomDataProcess, "roomData-" + room.name, 99, {
+                    roomName: room.name
+                });
+
+                if (!proc.kernel.getProcessByName("em-" + room.name))
+                {
+                    proc.kernel.addProcess(EnergyManagementProcess, "em-" + room.name, 50, {
+                        roomName: room.name
+                    });
+                }
+
+                if (!proc.kernel.hasProcess("sm-" + room.name))
+                {
+                    proc.kernel.addProcess(StructureManagementProcess, "sm-" + room.name, 48, {
+                        roomName: room.name
+                    });
+                }
+            }
         });
-      }
 
-      if (!proc.kernel.hasProcess("sm-" + room.name))
-      {
-        proc.kernel.addProcess(StructureManagementProcess, "sm-" + room.name, 48, {
-          roomName: room.name
-        });
-      }
-    });
+        this.kernel.addProcess(SuspensionProcess, "suspension-master", 99, { master: true });
+        this.kernel.addProcess(FlagWatcherProcess, "flag-watcher", 98, {});
 
-    this.kernel.addProcess(SuspensionProcess, "suspension-master", 99, { master: true });
-    this.kernel.addProcess(FlagWatcherProcess, "flag-watcher", 98, {});
-
-    this.completed = true;
-  }
+        this.completed = true;
+    }
 }

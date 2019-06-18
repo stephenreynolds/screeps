dbf6f860d4d688dc91ad3fd713bdbeb79a01c2a2
Stephen Reynolds <git@stephenreynolds.me>
2019-06-18 03:18:33 -0400

    Harvester role

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { ErrorMapper } from "utils/ErrorMapper";
+import { Harvester } from "roles/roleHarvester";
 
-// When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
-// This utility uses source maps to get the line numbers and file names of the original, TS source code
-export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
-
-  // Automatically delete memory of missing creeps
-  for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
-      delete Memory.creeps[name];
+export const loop = ErrorMapper.wrapLoop(() =>
+{
+    // Automatically delete memory of missing creeps
+    for (const name in Memory.creeps)
+    {
+        if (!(name in Game.creeps))
+        {
+            delete Memory.creeps[name];
+        }
     }
-  }
+
+    for (let name in Game.creeps)
+    {
+        let creep = Game.creeps[name];
+        Harvester.run(creep);
+    }
 });
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,20 +1,24 @@
 // example declaration file - remove these and add your own custom typings
 
 // memory extension samples
-interface CreepMemory {
-  role: string;
-  room: string;
-  working: boolean;
+interface CreepMemory
+{
+    role: string;
+    room: string;
+    working: boolean;
 }
 
-interface Memory {
-  uuid: number;
-  log: any;
+interface Memory
+{
+    uuid: number;
+    log: any;
 }
 
 // `global` extension samples
-declare namespace NodeJS {
-  interface Global {
-    log: any;
-  }
+declare namespace NodeJS
+{
+    interface Global
+    {
+        log: any;
+    }
 }

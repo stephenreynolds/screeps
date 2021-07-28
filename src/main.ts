823f35b8b3a3049b4803a24b78ddcd213ee97539
Stephen Reynolds <git@stephenreynolds.dev>
2021-07-27 20:37:12 -0400

    Add main.ts with ErrorMapper

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
-function main() {
+import {ErrorMapper} from "./utils/errorMapper";
 
-}
+export const loop = ErrorMapper.wrapLoop(() => {
+  // Delete memory of missing creeps
+  for (const name in Memory.creeps) {
+    if (!(name in Game.creeps)) {
+      delete Memory.creeps[name];
+    }
+  }
+});

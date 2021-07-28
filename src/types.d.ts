83f13e33d7cc0922d4d39c0109879292d7d8de03
Stephen Reynolds <git@stephenreynolds.dev>
2021-07-27 20:58:11 -0400

    Add types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-underscore-dangle */
+declare const __REVISION__: string;
+declare const __BRANCH__: string;
+declare const __DATE__: string;
+declare const __MESSAGE__: string;
+declare const __SCRIPT_BRANCH__: string;
+
+declare namespace NodeJS {
+  interface Global {
+    Memory: Memory
+  }
+}

125a9a67a908917b3a2cea769cde937c65c79464
Stephen Reynolds <git@stephenreynolds.dev>
2021-07-27 20:12:00 -0400

    Add rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+"use strict";
+
+import clear from "rollup-plugin-clear";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import replace from "rollup-plugin-replace";
+import screeps from "rollup-plugin-screeps";
+import git from "git-rev-sync";
+
+let cfg;
+const dest = process.env.DEST;
+if (!dest) {
+    console.log("No destination specified - code will be compiled but not uploaded");
+} else if ((cfg = require("./screeps.json")[dest]) == null) {
+    throw new Error("Invalid upload destination");
+}
+
+export default {
+    input: "src/main.ts",
+    output: {
+        file: "dist/main.js",
+        format: "cjs",
+        sourcemap: true
+    },
+
+    plugins: [
+        clear({
+            targets: ["dist"]
+        }),
+        resolve(),
+        commonjs(),
+        replace({
+            __REVISION__: JSON.stringify(git.short()),
+            __BRANCH__: JSON.stringify(git.branch()),
+            __DATE__: JSON.stringify(git.date().toLocaleDateString()),
+            __MESSAGE__: JSON.stringify(git.message()),
+            __SCRIPT_BRANCH__: JSON.stringify(dest)
+        }),
+        typescript({
+            tsconfig: "./tsconfig.json"
+        }),
+        screeps({
+            config: cfg,
+            dryRun: cfg == null
+        })
+    ]
+}
